9f8d51a150b964a6b3449434fda78e9bed0c3a87
yahia008 <yahyatijjani99@gmail.com>
2024-06-13 01:46:37 -0700

    hello

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,7 @@
-# Grant the user holberton permission to log in and access files without encountering any errors.
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
-# Raise the hard file limit for the Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
-}
-
-# Raise the soft file limit for the Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
